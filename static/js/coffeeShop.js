91f3aa79f033eaa8e21ad1f2333132fc12aceca0
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-29 16:09:23 -0700

    addItemToCart Event Handler

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Gets items by the id cart-items and is adding it to itemName
 const addItemToCart = (itemName) => {
   $('#cart-items').append(`
     <tr>
@@ -8,11 +9,14 @@ const addItemToCart = (itemName) => {
   `);
 };
 
+// This function is looking at our cart-total at the localhost and then setting it to empty
 const resetCart = () => {
   $('#cart-total').html('0.00');
   $('#cart-items').empty();
 };
 
+
+// This is adding the total to the price in the cart.
 const incrementCartTotal = (price) => {
   const cartTotal = $('#cart-total');
 
@@ -22,6 +26,8 @@ const incrementCartTotal = (price) => {
   cartTotal.html(total.toFixed(2));
 };
 
+
+//idk
 const incrementCoffeeSold = (amountSold) => {
   let coffeeSold = Number($('#coffee-sold-counter').html());
   coffeeSold += amountSold;
@@ -29,6 +35,8 @@ const incrementCoffeeSold = (amountSold) => {
   $('#coffee-sold-counter').html(coffeeSold);
 };
 
+
+//idk
 const setProgressAndStatus = (progressVal, statusMsg) => {
   $('#order-progress').attr('value', progressVal);
   $('#order-status-message').html(statusMsg);
@@ -38,3 +46,17 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+
+
+$('.add-to-order').on('click', () => {
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+});
+
+
+// Questions:
+// when they want us to type the function name in the console make sure im doing it right
+// WHat is last line doing in incrementCartTotal
+
+
+
